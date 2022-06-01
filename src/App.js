f66555c2577bd6cd887c734d04cf26e2162583e9
agumartinez1209 <agu.am.2359@gmail.com>
2022-06-01 20:26:21 -0300

    agrego componente con props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar";
+import Lista from "./components/Lista";
 
 
 function App() {
   return <div>
 
       <NavBar />
+      <Lista titulo={"NUESTROS PRODUCTOS"} />
   </div>;
 }
 
