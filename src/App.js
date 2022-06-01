47e173e4ec9b935ff453ef6661205f1ff42903ed
agumartinez1209 <agu.am.2359@gmail.com>
2022-06-01 18:50:03 -0300

    agregando el cartwidget

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import NavBar from "./components/NavBar";
 
 
 function App() {
-  return <div>aguante react
+  return <div>
 
       <NavBar />
-
   </div>;
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
+import CartWidget from "./CartWidget";
+
 function NavBar() {
     return  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html"><img src="../img/textotitulo.png" width="100" height="40"></img></a>
+        <a class="navbar-brand" href="index.html"><img src=".../img/textotitulo.png" width="100" height="40"></img></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"> </span>
@@ -9,22 +11,22 @@ function NavBar() {
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">inicio</a>
+                    <a class="nav-link active" aria-current="page" href="index.html">INICIO</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" >nosotros</a>
+                    <a class="nav-link" >NOSOTROS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" >novedades</a>
+                    <a class="nav-link" >NOVEDADES</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" >productos</a>
+                    <a class="nav-link" >PRODUCTOS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" >contacto</a>
+                    <a class="nav-link" >CONTACTO</a>
                 </li>
             </ul>
-
+            <CartWidget />
         </div>
     </div>
 </nav>
