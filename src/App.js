ec7d1cd45374c43dabd9d551e6ddfb09cb623da4
agumartinez1209 <agu.am.2359@gmail.com>
2022-06-06 23:08:16 -0300

    modificando el class y colocando el contador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import NavBar from "./components/NavBar";
 import Lista from "./components/Lista";
+import ItemCount from "./components/ItemCount";
 
 
 function App() {
-  return <div>
+  
+  const onAdd = (count) => {
+    alert(count);
+
+  }
+  
+  return (<div>
 
       <NavBar />
       <Lista titulo={"NUESTROS PRODUCTOS"} />
-  </div>;
+      <ItemCount inicial ={1} maximo = {20} onAdd ={onAdd} />
+  </div>
+  );
 }
 
 export default App;
--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,8 +1,8 @@
 
 
 export default function Lista(props) {
-  return <div > <h1 class="text-center mb-5 shadow-sm p-3 color">{props.titulo}</h1>
-  <p class="text-center">vendemos parafernalia para potenciar la experiencia del humo, llevandola a otro
+  return <div > <h1 className="text-center mb-5 shadow-sm p-3 color">{props.titulo}</h1>
+  <p className="text-center">vendemos parafernalia para potenciar la experiencia del humo, llevandola a otro
             nivel. Nos importa la
             calida de lo que vendemos y consumimos: queremos solo lo mejor. productos finos, funcionales y tambien
             bellos para honrar el acto ancestral de fumar. Nuestra busqueda es crear una experiencia integral donde
