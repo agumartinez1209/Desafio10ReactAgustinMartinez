1bed3accb28b452d9396a248bece8111929d25d3
agumartinez1209 <agu.am.2359@gmail.com>
2022-06-20 17:17:21 -0300

    boton finalizar y cart

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -36,53 +35,16 @@ const onAdd = (count) => {
                     <p className="card-text price-detail  mb-5 shadow-sm p-3 ">Precio: ${precio}</p>
                     {unidades >0? <Link to={"/cart"} className="btn btn-outline-success"> Finalizar compra </Link>:<ItemCount inicial ={1} maximo = {20} onAdd ={onAdd}/> }
 
-=======
-import React from 'react'
-
-const ItemDetail = ({id, name, image, species, status}) => {
-    return (
-        <>
-    
-          <div className="container container-detail">
-            <div className="row">
-              <div className="col-12 col-sm-6 pt-5">
-                <div className="card mb-3 card-style">
-                  <div className="row g-0">
-                    <div className="col-md-4">
-                      <img src={image} className="img-fluid img-detail" />
-                    </div>
-                    <div className="col-md-8 col-sm-6 pt-5">
-                      <div className="card-body">
-                        <h5 className="card-title card-title-detail">{name}</h5>
-                        <p className="card-text card-detail mt-3">{species}</p>
-                        <p className="card-text card-detail mt-3">{status}</p>
-                        
-                        
-                        <div className="d-flex gap-3 justify-content-center mt-3">
-                            <button  type="button" className="btn card-btn">-</button>
-                            <h2>1 </h2>
-                            <button type="button" className=" btn card-btn">+</button>
-                        </div>
-                        <button type="button" className="btn card-btn-cart d-grid gap-2 col-3 mx-auto mt-5">Agregar al Carrito</button>
-                      </div>
-                    </div>
->>>>>>> 0be3c50ae0c2723522ed2c5a4a17005d5bd43e90
                   </div>
                 </div>
               </div>
             </div>
           </div>
-<<<<<<< HEAD
         </div>
       </div>
 
 
     </>
-=======
-    
-    
-        </>
->>>>>>> 0be3c50ae0c2723522ed2c5a4a17005d5bd43e90
       );
     };
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -9,21 +9,12 @@ function ItemListContainer ({categoria, onAdd }) {
 
   useEffect( () => {
     setTimeout(()=>{
-<<<<<<< HEAD
       fetch("../../productos.json")
         .then (res => res.json ())
         .then (data => {
           setProductos(data)  })  
         .catch (error => console.error("error:", error))
     },2000)
-=======
-      fetch("https://rickandmortyapi.com/api/character")
-      .then (res => res.json ())
-      .then (res => setProductos(res.results))  
-      .catch (error => console.error("error:", error))
-    },2000)
-
->>>>>>> 0be3c50ae0c2723522ed2c5a4a17005d5bd43e90
 
 
     },[categoria])
